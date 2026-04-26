6f7e5cd35c615c590951c08b4c9d8d112a0a7db0
m.AnimeRTR <subanime956@gmail.com>
2026-04-26 01:04:09 -0500

    Update Onesignalpush.js

--- assets/js/Onesignalpush.js
+++ assets/js/Onesignalpush.js
@@ -1,6 +1,6 @@
   window.OneSignalDeferred = window.OneSignalDeferred || [];
   OneSignalDeferred.push(async function(OneSignal) {
     await OneSignal.init({
-      appId: "ff116a46-1868-416b-920c-abd03794dcdb",
+      appId: "8a8ebd1a-f5f1-43f7-b3a3-daff7d1390b5",
     });
   });
